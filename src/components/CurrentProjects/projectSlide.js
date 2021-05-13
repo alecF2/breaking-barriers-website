13311251aa59dea1b270d854ece34ff21bd0623b
Iftekhar016 <72429276+Iftekhar016@users.noreply.github.com>
2021-05-12 20:10:46 -0700

    installed react-icons, react-slick for slides

--- src/components/CurrentProjects/projectSlide.js
+++ src/components/CurrentProjects/projectSlide.js
@@ -1,12 +1,20 @@
 import './projectSlide.css';
-
-import React, { useEffect } from 'react';
+import Slider from "react-slick";
+import React, { useEffect, useState } from 'react';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
 
 const ProjectSlide = () =>{
+
+	const [current, setCurrent] = useState(0);
+
 	return(
 		<React.Fragment>
 			<div className="barriers-project-slide">
 				<h1>This is for Ethan, Iftekar, and Raunak!</h1>
+				<h1> This is inside slides</h1>
+				
+				<FaArrowAltCircleLeft className='left-arrow'/>
+				<FaArrowAltCircleRight className='right-arrow'/>
 			</div>
 		</React.Fragment>
 	);
