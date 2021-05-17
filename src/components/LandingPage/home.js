06ffe3a4643e720f98cc1dc417f81cd549ddedb2
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-16 20:38:41 -0700

    Changed text in home.js

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -6,7 +6,9 @@ const Home = () =>{
 	return(
 		<React.Fragment>
 			<div className="barriers-home">
-				<h1>This is Arnav and Amy!</h1>
+			<img src="pic_trulli.jpg" alt="Italian Trulli"></img>
+				<h1><q>Breaking</q> that invisible barrier across generations</h1>
+					<h2>smaller text goes here</h2>
 			</div>
 		</React.Fragment>
 	);
