1db47528885b389360cb4ca4676655339ef35938
Raunak Anand <rsanand@ucdavis.edu>
2021-05-19 22:47:02 +0530

    Created navbar and footer

---
+++ src/components/Navbar/RightNav.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Ul = styled.ul`
+	margin-top: 0px;
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+
+  li {
+    padding: 30px 25px;
+  }
+  li a {
+    font-family: 'M PLUS Rounded 1c';
+    font-weight: 500;
+    font-size: 18px;
+    color: #37392E;
+    text-decoration: none;
+  }
+  li a:hover {
+    color: 37392E;
+    text-decoration: underline;
+    text-decoration-color: #37392E;
+  }
+  @media (max-width: 768px) {
+    margin-top: 90px;
+    flex-flow: column nowrap;
+    background-color: #fff;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(-150%)'};
+    top: 0;
+    right: 0;
+    height: 42vh;
+    width: 100vw; 
+    padding-top: 2rem;
+    transition: transform 0.5s ease-in-out;
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <Ul open={open}>
+      <li><a href="/">Home</a></li>
+      <li><a href="/team">Our Team</a></li>
+      <li><a href="/currentProjects">Current Projects</a></li>
+      <li><a href="/contactUs">Contact Us</a></li>
+    </Ul>
+  )
+}
+
+export default RightNav;
