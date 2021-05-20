3a785f040c347263cc0a69b1e6abe68eba7f4999
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-19 21:12:41 -0700

    Changed css, changed absolute width to percentage

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -9,11 +9,8 @@ const Home = () =>{
 			<img className="home-img" src="image.png" alt="Landing"></img>
 			<div className="overlay-text">
 				<h1><q>Breaking</q> that invisible barrier across generations</h1>
-				<h2></h2>
-			</div>
-			</div>
-			<div>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
+			</div>
 			</div>
 		</React.Fragment>
 	);
