353132081c541112ba4dfe3bd0ea129697ed4d7e
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-19 20:44:54 -0700

    Changed text-align and added button

--- src/components/LandingPage/mission.js
+++ src/components/LandingPage/mission.js
@@ -8,6 +8,7 @@ const Mission = () =>{
 			<div className="mission-main">
 				<h1>Our Mission Statement</h1>
 				<h2>Our ultimate goal is to bridge language and cultural barriers within the underserved elderly population as well as inspire a new sense of purpose in college students as they engage and learn from elderly adults.</h2>
+				<button class="mission-learn-more">Learn More ></button>
 			</div>
 		</React.Fragment>
 	);
