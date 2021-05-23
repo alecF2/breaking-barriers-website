77a39622ab879617d25aaf91212483527d6e1b88
devin177 <ly.devin.177@gmail.com>
2021-05-22 20:31:32 -0700

    Refactored captcha and contact_us route into one

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "assets")));
 
 // instance of nodemailer
 const nodemailer = require("nodemailer");
@@ -29,54 +29,48 @@ let transporter = nodemailer.createTransport({
 //     res.sendFile(__dirname + '/index.html');
 // });
 
-app.post("/contact_us", (req, res) => {
-  // judge the score
-  // Change it so front end only does 1 API call
-  const mailOptions = {
-    to: process.env.SUPPORT_EMAIL,
-    subject: `${req.body.name}: ${req.body.subject}`,
-    replyTo: req.body.email
-  }
-
-  if (req.body.score <= 0.3) {
-    // end
-    ;
-  } else if (req.body.score <= 0.7) {
-    // send with disclaimer
-    mailOptions.text = "Warning from server: Potential spam\n" + req.body.message;
-    transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          console.log(error);  
-          res.json(error);
-      }
-      else{
-          console.log('sent' + info.response);
-          res.json("Successfully sent email to " + mailOptions.to);
-      }
-    })
-  } else {
-    // 0.7+
-    mailOptions.text = req.body.message;
-    transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-          console.log(error);  
-          res.json(error);
-      }
-      else{
-          console.log('sent' + info.response);
-          res.json("Successfully sent email to " + mailOptions.to);
-      }
-    })
-  }
-
-});
-
 app.post("/captcha", (req, res) => {
+  // Sending token to get verified
   fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_CAPTCHA_KEY}&response=${req.body.token}`, {
     method: 'POST',
     headers: {'Content-Type':'application/json'}
   }).then(response => response.json()).then(data => {
-    console.log(data);
+    const mailOptions = {
+      to: process.env.SUPPORT_EMAIL,
+      subject: `${req.body.name}: ${req.body.subject}`,
+      replyTo: req.body.email
+    }
+  
+    if (data.score <= 0.3) {
+      // end
+      ;
+    } else if (data.score <= 0.7) {
+      // send with disclaimer
+      mailOptions.text = "Warning from server: Potential spam\n" + req.body.message;
+      transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);  
+            res.json(error);
+        }
+        else{
+            console.log('sent' + info.response);
+            res.json("Successfully sent email to " + mailOptions.to);
+        }
+      })
+    } else {
+      // 0.7+
+      mailOptions.text = req.body.message;
+      transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);  
+            res.json(error);
+        }
+        else{
+            console.log('sent' + info.response);
+            res.json("Successfully sent email to " + mailOptions.to);
+        }
+      })
+    }
   })
 });
 
