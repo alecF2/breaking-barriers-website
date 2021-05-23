85bf95f77f45bb8b909f92277bb6d138701cd6d3
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-22 19:00:01 -0700

    Removed image for now

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -6,12 +6,12 @@ const Home = () =>{
 	return(
 		<React.Fragment>
 			<div className="barriers-home">
-			<img className="home-img" src="image.png" alt="Landing"></img>
 				<h1><q>Breaking</q> that <i>invisible</i> barrier across generations</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
 			</div>
 		</React.Fragment>
 	);
 }
+//			<img className="home-img" src="image.png" alt="Landing"></img>
 
 export default Home;
