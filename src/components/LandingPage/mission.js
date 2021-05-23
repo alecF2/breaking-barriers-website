b67d4cf8be0c18f089b3a4be67dfddb37dd16588
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-22 19:07:36 -0700

    added semi-circles

--- src/components/LandingPage/mission.js
+++ src/components/LandingPage/mission.js
@@ -10,6 +10,9 @@ const Mission = () =>{
 				<h2>Our ultimate goal is to <strong>bridge language and cultural barriers within the underserved elderly population</strong> as well as <strong>inspire a new sense of purpose in college students</strong> as they engage and learn from elderly adults.</h2>
 				<button class="volunteer">volunteer with us!</button>
 			</div>
+			<div class="circle"></div>
+			<div class="circle2"></div>
+
 		</React.Fragment>
 	);
 }
