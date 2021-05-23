b6e02d3c5f32e039c681c66ddcedf39dd8252448
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-22 18:58:20 -0700

    minor css updates

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -7,10 +7,8 @@ const Home = () =>{
 		<React.Fragment>
 			<div className="barriers-home">
 			<img className="home-img" src="image.png" alt="Landing"></img>
-			<div className="overlay-text">
-				<h1><q>Breaking</q> that invisible barrier across generations</h1>
+				<h1><q>Breaking</q> that <i>invisible</i> barrier across generations</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
-			</div>
 			</div>
 		</React.Fragment>
 	);
--- src/components/LandingPage/mission.js
+++ src/components/LandingPage/mission.js
@@ -7,8 +7,8 @@ const Mission = () =>{
 		<React.Fragment>
 			<div className="mission-main">
 				<h1>Our Mission Statement</h1>
-				<h2>Our ultimate goal is to bridge language and cultural barriers within the underserved elderly population as well as inspire a new sense of purpose in college students as they engage and learn from elderly adults.</h2>
-				<button class="learn-more">LEARN MORE ></button>
+				<h2>Our ultimate goal is to <strong>bridge language and cultural barriers within the underserved elderly population</strong> as well as <strong>inspire a new sense of purpose in college students</strong> as they engage and learn from elderly adults.</h2>
+				<button class="volunteer">volunteer with us!</button>
 			</div>
 		</React.Fragment>
 	);
