e68a6dceea7e9199745c9e75759757a7b9f120e2
Iftekhar016 <72429276+Iftekhar016@users.noreply.github.com>
2021-05-23 21:01:30 -0700

    changed image paths

--- src/components/CurrentProjects/SliderData.js
+++ src/components/CurrentProjects/SliderData.js
@@ -4,23 +4,23 @@ export const SliderData = [
         titleText: 'Technology:',
         CPsubtitle: 'Breaking Barriers have partnered with ACC Senior Services to provide technology support and lessons to older adults.',
         CPCaption: 'ACC Senior Services is a local organization founded to “provide housing, education, and social services to help older adults navigate and sustain an independent lifestyle. We have particularly partnered with their Senior Community Service Employment Program (SCSEP) to deliver tailored technology lessons to their older adults. We partner a SCSEP participant with one Breaking Barriers volunteer who will meet with them weekly to provide technology lessons. These lessons are participant-specific so that they are able to learn skills they are interested in. During COVID-19, our volunteers have met on Zoom with their SCSEP partner to have their weekly tailored-lessons.',
-        CPBimg1:'images\CPGroup120.svg',
-        CPBimg2:'images\CPEllipse69.svg'
+        CPBimg1:'images/CPGroup120.svg',
+        CPBimg2:'images/CPEllipse69.svg'
     },
     {
         CPimage: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
         titleText: '"Pen"pals:',
         CPsubtitle: 'Breaking Barriers have partnered with local facilities to connect our college student volunteers with elderly residents',
         CPCaption: 'Just as our motto goes, we would like to “break” that generational barrier that exists between the younger and older generations. To do so, we have partnered with various senior facilities to connect our volunteers with elderly residents who have little opportunity to interact with younger people. We firmly believe that despite the age and generational gap between our volunteers and residents, there is much to be shared and learned from each other. During COVID-19, our volunteers typically schedule weekly ~1 hour phone calls with their resident to chat or even watch movies with. We try our best to match our volunteers with residents who have similar interests, hobbies, culture, and language - eliminating any potential “barriers',
-        CPBimg1:'images\CPGroup121.svg',
-        CPBimg2:'images\CPEllipse69_2.svg'
+        CPBimg1:'images/CPGroup121.svg',
+        CPBimg2:'images/CPEllipse69_2.svg'
     },
     {
         CPimage: 'https://images.unsplash.com/photo-1510446418220-00d2e0a27795?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
         titleText: 'Arts & Crafts:',
         CPsubtitle: 'Breaking Barriers have partnered with local facilities to deliver arts and crafts to residents at senior centers.',
         CPCaption: 'Our volunteers are very talented in arts and crafts, ranging from painting to origami. With these various skills, we have developed the Arts & Crafts program where our volunteers teach or present a DIY/Arts program to elderly adults staying at a senior living center. We typically host these per request of the facility and based on the resources they are able to provide. During COVID-19, we have delivered live Zoom presentations that include live poetry/storytelling produced and presented by our volunteers',
-        CPBimg1:'images\CPGroup122.svg',
-        CPBimg2:'images\CPEllipse71.svg'
+        CPBimg1:'images/CPGroup122.svg',
+        CPBimg2:'images/CPEllipse71.svg'
     }
 ]
