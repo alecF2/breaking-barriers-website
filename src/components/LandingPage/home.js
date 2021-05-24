aa2c7476a1772031256a88dde8ca7419a96230ff
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-23 21:07:26 -0700

    Changed image names, added images to pages

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -6,11 +6,11 @@ const Home = () =>{
 	return(
 		<React.Fragment>
 			<div className="barriers-home">
-				<h1><q>Breaking</q> that invisible barrier across generations</h1>
+				<h1><q>Breaking</q> that <i>invisible</i> barrier across generations</h1>
+				<img src="images/circles/LPhomeIMG.svg" class="first"></img>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
 			</div>
-			<div class="circle3"></div>
-			<div class="circle4"></div>
+			<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 		</React.Fragment>
 	);
 }
--- src/components/LandingPage/whoWeAre.js
+++ src/components/LandingPage/whoWeAre.js
@@ -7,10 +7,10 @@ const WhoWeAre = () =>{
 		<React.Fragment>
 			<div className="barriers-who-we-are">
 				<h1>Who We Are</h1>
-				<img src="images/circles/Rectangle 600.svg"></img><br></br>
-					<img src="images/circles/Ellipse 17.svg" class="first"></img>
-					<img src="images/circles/Ellipse 52 (1).svg" class="second"></img>
-					<img src="images/circles/Ellipse 52 (1).svg" class="second"></img>
+				<img src="images/circles/LPRectangle600.svg"></img><br></br>
+					<img src="images/circles/LPEllipse17.svg" class="first"></img>
+					<img src="images/circles/LPEllipse52(1).svg" class="second"></img>
+					<img src="images/circles/LPEllipse52(1).svg" class="second"></img>
 					<img src="images/circles/Ellipse 52 (1).svg" class="second"></img>
 				<h2>We are a <strong>student-led organization</strong> at UC Davis seeking to build lasting relationships with older adults through various projects. The gap between older adults and the younger generations does not get any smaller as time goes on.</h2>
 				<button class="learn-more" onclick="">learn more ></button>
