cc10b0822a3cdc552d134883e298884fa3fa5342
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-23 20:59:29 -0700

    Added images

--- src/components/LandingPage/mission.js
+++ src/components/LandingPage/mission.js
@@ -7,6 +7,7 @@ const Mission = () =>{
 		<React.Fragment>
 			<div className="mission-main">
 				<h1>Our Mission Statement</h1>
+				<img src="images/circles/Rectangle 500.svg"></img><br></br>
 				<h2>Our ultimate goal is to <strong>bridge language and cultural barriers within the underserved elderly population</strong> as well as <strong>inspire a new sense of purpose in college students</strong> as they engage and learn from elderly adults.</h2>
 				<button class="volunteer">volunteer with us!</button>
 			</div>
--- src/components/LandingPage/whoWeAre.js
+++ src/components/LandingPage/whoWeAre.js
@@ -7,8 +7,13 @@ const WhoWeAre = () =>{
 		<React.Fragment>
 			<div className="barriers-who-we-are">
 				<h1>Who We Are</h1>
+				<img src="images/circles/Rectangle 600.svg"></img><br></br>
+					<img src="images/circles/Ellipse 17.svg" class="first"></img>
+					<img src="images/circles/Ellipse 52 (1).svg" class="second"></img>
+					<img src="images/circles/Ellipse 52 (1).svg" class="second"></img>
+					<img src="images/circles/Ellipse 52 (1).svg" class="second"></img>
 				<h2>We are a <strong>student-led organization</strong> at UC Davis seeking to build lasting relationships with older adults through various projects. The gap between older adults and the younger generations does not get any smaller as time goes on.</h2>
-				<button class="learn-more">learn more ></button>
+				<button class="learn-more" onclick="">learn more ></button>
 			</div>
 			<div className="barriers-tidbits-about-us">
 				<h1>Tidbits About Us</h1>
