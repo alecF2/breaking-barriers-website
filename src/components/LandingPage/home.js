7816d9a21f55a14f998b701523ffccd01bca074e
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-23 21:23:34 -0700

    minor css changes

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -5,12 +5,12 @@ import React, { useEffect } from 'react';
 const Home = () =>{
 	return(
 		<React.Fragment>
+			<img src="images/circles/LPhomeIMG.svg" class="first"></img>
 			<div className="barriers-home">
 				<h1><q>Breaking</q> that <i>invisible</i> barrier across generations</h1>
-				<img src="images/circles/LPhomeIMG.svg" class="first"></img>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
 			</div>
-			<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+			<br></br><br></br><br></br><br></br><br></br><br></br>
 		</React.Fragment>
 	);
 }
