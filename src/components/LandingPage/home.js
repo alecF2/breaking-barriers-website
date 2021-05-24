00055e9d5318c0be1df08fc808fd9450be1e313d
jayamy04 <amylee041182@gmail.com>
2021-05-23 20:51:40 -0700

    Added Tidbits

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -6,7 +6,7 @@ const Home = () =>{
 	return(
 		<React.Fragment>
 			<div className="barriers-home">
-				<h1><q>Breaking</q> that <i>invisible</i> barrier across generations</h1>
+				<h1><q>Breaking</q> that invisible barrier across generations</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
 			</div>
 			<div class="circle3"></div>
