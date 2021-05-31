6b3700d20f56923feead414e1da40e0285bb19f9
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-05-30 19:35:14 -0700

    Changed landing image for desktop

--- src/components/LandingPage/home.js
+++ src/components/LandingPage/home.js
@@ -6,7 +6,7 @@ const Home = () =>{
 	return(
 		<React.Fragment>
 			<img src="images/circles/LPhomeIMG.svg" class="first"></img>
-			<img src="images/random.svg" class="desktopCover"></img>
+			<img src="images/LPlanding.svg" class="desktopCover"></img>
 			<div className="barriers-home">
 				<h1 class="overlay-text"><q>Breaking</q> that <i>invisible</i> barrier across generations</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</p>
