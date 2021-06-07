714d277445581584f7635cc048c62e4d9a0224ac
Raunak Anand <rsanand@ucdavis.edu>
2021-06-06 19:52:20 -0700

    updated current projects page

---
+++ src/components/CurrentProjects/cp_contact.js
@@ -0,0 +1,24 @@
+import './cp_contact.css';
+
+import React, { useEffect } from 'react';
+
+const CP_Contact = () =>{
+	return(
+		<React.Fragment>
+			<div className="cp-contact">
+				<div className="cp-contact-text">
+					We are always looking for new
+					partnerships with local facilities! As an
+					organization, we would love to reach out
+					and “break” generational barriers
+					wherever we go. If you are a facility 
+					interested in partnering with, please do 
+					not hesitate to contact us!
+				</div>
+				<button>Contact Us</button>
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default CP_Contact;
--- src/components/CurrentProjects/currentProjects_body.js
+++ src/components/CurrentProjects/currentProjects_body.js
@@ -6,24 +6,26 @@ const CurrentProjects_body = () =>{
 	return(
 		<React.Fragment>
 			<div className="barriers-current-proj-body web">
+				<div className="project-row">
+					<div className="current-proj-pic">
+						<img id="cp-cat" src="images/CPcat.svg"/>
+						<img id="cp-frame" src="images/Frame.svg"/>
+					</div>
+					<div className="current-proj-text">
+						<h1 id="current-proj-title">Current Projects</h1>
 
-				<div className="current-proj-pic">
-					<img id="cp-cat" src="images/CPcat.svg"/>
-					<img id="cp-frame" src="images/Frame.svg"/>
+						<a>We have partnered up with various local
+						<br/>facilities to provide different programs to
+						<br/>older adults. These local facilities have
+						<br/>different objectives and we have partnered
+						<br/>with them to further enhance the quality of
+						<br/>programming they have for their residents.
+						<br/>Some programs we are currently involved with are:</a>
+					</div>
 				</div>
-				<div className="current-proj-text">
-					<h1 id="current-proj-title">Current Projects</h1>
-
-					<a>We have partnered up with various local
-					<br/>facilities to provide different programs to
-					<br/>older adults. These local facilities have
-					<br/>different objectives and we have partnered
-					<br/>with them to further enhance the quality of
-					<br/>programming they have for their residents.
-					<br/>Some programs we are currently involved with are:</a>
-				</div>
-
+				<a className="arrow-head">&#8964;</a>
 			</div>
+
 			<div className="barriers-current-proj-body mobile">
 			
 				<div className="current-proj-pic">
@@ -36,12 +38,14 @@ const CurrentProjects_body = () =>{
 				<h1 id="current-proj-title">Current Projects</h1>
 
 				<a>We have partnered up with various local
-				<br/>facilities to provide different programs to
-				<br/>older adults. These local facilities have
-				<br/>different objectives and we have partnered
-				<br/>with them to further enhance the quality of
-				<br/>programming they have for their residents.
-				<br/>Some programs we are currently involved with are:</a>
+				facilities to provide different programs to
+				older adults. These local facilities have
+				different objectives and we have partnered
+				with them to further enhance the quality of
+				programming they have for their residents.
+				Some programs we are currently involved with are:</a>
+
+				<a className="arrow-head">&#8964;</a>
 			</div>
 		</React.Fragment>
 	);
