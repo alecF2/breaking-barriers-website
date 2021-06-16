b7b267d4486db6bb6cc60d868466b809194b3995
Arnav33R <59548836+Arnav33R@users.noreply.github.com>
2021-06-17 01:59:49 +0530

    Added flexbox for image+frames

--- src/components/LandingPage/whoWeAre.js
+++ src/components/LandingPage/whoWeAre.js
@@ -7,9 +7,10 @@ const WhoWeAre = () =>{
 		<React.Fragment>
 			<div className="barriers-who-we-are">
 				<h1>Who We Are</h1>
-				<img src="images/WhoWeAre.svg" class="dog"></img><br></br>
-				<img src="images/landpage_whoweare.svg"></img><br></br>
-					
+				<div className="first-frame">
+					<img src="images/WhoWeAre.svg" class="dog"></img><br></br>
+					<img src="images/circles/LPRectangle599.svg" class="wframe"></img><br></br>
+				</div>
 				<h2>We are a <strong>student-led organization</strong> at UC Davis seeking to build lasting relationships with older adults through various projects. The gap between older adults and the younger generations does not get any smaller as time goes on.</h2>
 				<button class="learn-more" onclick="">learn more </button>
 			</div>
@@ -18,4 +19,4 @@ const WhoWeAre = () =>{
 	);
 }
 
-export default WhoWeAre;
+export default WhoWeAre; 
